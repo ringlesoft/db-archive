d3af90aa3226667550a8edf01add0c0cd221ab90
David Ringle <ringunger@gmail.com>
2025-03-08 18:43:32 +0300

    Additional Trait methods

--- src/Traits/ArchivesData.php
+++ src/Traits/ArchivesData.php
@@ -21,4 +21,28 @@ Trait ArchivesData
     }
 
 
+    /**
+     * Get archive settings for the model
+     * @return array
+     */
+    public static function getArchiveSettings(): array
+    {
+        $tableName = (new static())->getTable();
+        $defaultSettings = Config::get('db_archive.settings');
+        $tableSettings = Config::get('db_archive.tables') ?? [];
+        return array_merge($defaultSettings, $tableSettings[$tableName] ?? []);
+    }
+
+
+    /**
+     * Check if archiving is enabled for the model
+     * @return bool
+     */
+    public static function isArchivingEnabled(): bool
+    {
+        $tables = Config::get('db_archive.tables', []);
+       return array_key_exists((new static())->getTable(), $tables) || in_array((new static())->getTable(), $tables, true);
+    }
+
+
 }
